35a30b02778129cd6949c9d8756bde4849abd4ea
Juan Manuel Malaver <juanmalaver1511@gmail.com>
2026-06-29 11:41:32 -0500

    Cambio

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Lucid from "./pages/Lucid.jsx";
+import Lucid from "./pages/InspyraTech.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
